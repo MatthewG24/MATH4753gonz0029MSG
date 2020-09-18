aa23220cf2b0bd4f073420b33fd490f04231c57a
Matthew <matthew.s.gonzales-1@ou.edu>
2020-09-18 16:27:54 -0500

    lab4funcii

--- R/lab4func.R
+++ R/lab4func.R
@@ -6,5 +6,5 @@
 #' @export
 #'
 #' @examples
-#' {quad.lm<-lm(y~x,data=df);myplot(x=15)}
+#' {quad.lm<-lm(y~x,data=spruce);myplot(x=15)}
 myplot=function(x){quad.lm$coef[1]+quad.lm$coef[2]*x+quad.lm$coef[3]*x^2}
