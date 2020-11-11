150fe91b2f01cf285540eaa0c67684c85f31381f
Matthew Gonzales <matthew.s.gonzales-1@ou.edu>
2020-11-11 10:18:24 -0600

    pls work

---
+++ R/Confidence Interval Function.R
@@ -0,0 +1,12 @@
+#' @title Confidence Interval Function
+#'
+#' @param x a function
+#'
+#' @return confidence interval
+#' @export
+#'
+#' @examples
+#' myci(x=rnorm(30,10,5))
+myci=function(x=rnorm(30,mean=20,sd=5)){
+  t.test(x,conf.level=.95)$conf.int
+}
